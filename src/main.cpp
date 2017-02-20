b6903edea64980c6d75f404bd4de5d96a33da98c
JalenTheEpic <FearTheDurp@gmail.com>
2017-02-20 00:30:30 -0500

    Add console for Debug

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <core\math\Matrix3.h>
+int main()
+{
+	std::cout << "test" << std::endl;
+	Palette3D::Matrix3 m;
+	m = Palette3D::Matrix3::IDENTITY;
+	std::cout << m <<std::endl;
+	while(1)
+	{
+		;
+	}
+}
