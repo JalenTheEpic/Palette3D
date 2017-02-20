736bf3855b19bc2dc6dc3ef07408e60269b5aa52
JalenTheEpic <fearthedurp@gmail.com>
2017-02-19 20:58:13 -0500

    Add constructors for Matrix3.h

--- include/core/math/Matrix3.h
+++ include/core/math/Matrix3.h
@@ -1,3 +1,9 @@
+/*
+Palette3d
+Copyright (c) 2017 Jalen Cureton, William Buell
+Licensed under the terms of the MIT License (see LICENSE.txt)
+*/
+
 #ifndef _CORE_MATH_MATRIX3_
 #define _CORE_MATH_MATRIX3_
 
@@ -12,10 +18,12 @@ namespace Palette3D
 
 	public:
 		Matrix3();
+
 		Matrix3(float m[3][3])
 		{
 			memcpy(mMatrix, m, 9 * sizeof(float));
 		}
+
 		inline void swap(Matrix3& other)
 		{
 			std::swap(mMatrix[0][0], other.mMatrix[0][0]);
@@ -28,8 +36,31 @@ namespace Palette3D
 			std::swap(mMatrix[2][1], other.mMatrix[2][1]);
 			std::swap(mMatrix[2][2], other.mMatrix[2][2]);
 		}
+
+		Matrix3(float e00, float e01, float e02,
+				float e10, float e11, float e12,
+				float e20, float e21, float e22) 
+		{
+			mMatrix[0][0] = e00;
+			mMatrix[0][1] = e01;
+			mMatrix[0][2] = e02;
+			mMatrix[1][0] = e10;
+			mMatrix[1][1] = e11;
+			mMatrix[1][2] = e12;
+			mMatrix[2][0] = e20;
+			mMatrix[2][1] = e21;
+			mMatrix[2][2] = e22;
+		}
 		
-		~Matrix3();
+		~Matrix3()
+		{
+			delete[] mMatrix;
+		}
+
+
+		
+
+
 
 	};
 
