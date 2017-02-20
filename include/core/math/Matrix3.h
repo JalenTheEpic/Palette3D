27cf98e5a4192b283fe2463a6c0399ce848d9628
JalenTheEpic <FearTheDurp@gmail.com>
2017-02-19 19:47:13 -0500

    Update Matrix3.h

--- include/core/math/Matrix3.h
+++ include/core/math/Matrix3.h
@@ -1,1 +1,22 @@
-#pragma once
+#ifndef _CORE_MATH_MATRIX3_
+#define _CORE_MATH_MATRIX3_
+
+namespace Palette3D 
+{
+	class Vec3
+	{
+	public:
+		float mX;
+		float mY;
+		float mZ;
+
+	public:
+		Vec3();
+		~Vec3();
+
+	};
+
+}
+
+
+#endif
