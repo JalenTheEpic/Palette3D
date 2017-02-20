c64620868442e55f75f54171e6edb3b489217a28
JalenTheEpic <FearTheDurp@gmail.com>
2017-02-20 00:45:38 -0500

    Testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+#include <string>
 #include <core\math\Matrix3.h>
 int main()
 {
 	std::cout << "test" << std::endl;
-	Palette3D::Matrix3 m;
-	m = Palette3D::Matrix3::IDENTITY;
-	std::cout << m <<std::endl;
-	while(1)
+	Palette3D::Matrix3 m(1, 2, 3, 4, 5, 6, 7, 8, 9);
+	std::cout << m* m <<std::endl;
+
+	while (1)
 	{
 		;
 	}
+
+	return 0;
+	
+	
 }
