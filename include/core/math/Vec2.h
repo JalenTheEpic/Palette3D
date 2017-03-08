bc988e79da563217bf7d4fa5a4d4809d480ab7c0
JalenTheEpic <fearthedurp@gmail.com>
2017-03-08 00:57:23 -0500

    Add basic vector2 operations

--- include/core/math/Vec2.h
+++ include/core/math/Vec2.h
@@ -7,6 +7,8 @@ Licensed under the terms of the MIT License (see LICENSE.txt)
 #ifndef _CORE_MATH_VEC2_
 #define _CORE_MATH_VEC2_
 
+#include <iostream>
+
 namespace Palette3D 
 {
 	class Vec2
@@ -24,16 +26,30 @@ namespace Palette3D
 
 		/// Operator functions.
 	public:
-		Vec2 operator+(const Vec2& rhs) const;
-		Vec2 operator-(const Vec2& rhs) const;
-		Vec2 operator*(const float& rhs) const;
+		inline Vec2 operator+(const Vec2& rhs) const
+		{
+			return Vec2(mX + rhs.mX, mY + rhs.mY);
+		}
+		
+		inline Vec2 operator-(const Vec2& rhs) const
+		{
+			return Vec2(mX - rhs.mX, mY -  rhs.mY);
+		}
+		Vec2 operator*(const float& rhs) const
+		{
+			return Vec2(mX * rhs, mY * rhs);
+		}
 
-		Vec2 operator+=(const Vec2& rhs);
-		Vec2 operator-=(const Vec2& rhs);
-		Vec2 operator*=(const float& rhs);
+		void operator+=(const Vec2& rhs);
+		void operator-=(const Vec2& rhs);
+		void operator*=(const float& rhs);
 
-		Vec2 operator=(const Vec2& rhs);
-
+		inline void operator=(const Vec2& rhs) 
+		{
+			mX = rhs.mX;
+			mX = rhs.mY;
+		}
+		friend std::ostream& operator<< (std::ostream& os, const Vec2& v);
 		/// Methods.
 	public:
 		Vec2 cross(const Vec2& other) const;
@@ -41,7 +57,9 @@ namespace Palette3D
 		float magnitude() const;
 		float squaredMagnitude() const;
 		float distance(const Vec2& other) const;
-		float squaredDistance() const;
+		float squaredDistance(const Vec2& other) const;
+
+		friend Vec3;
 	};
 
 }
--- src/core/math/Vec2.cpp
+++ src/core/math/Vec2.cpp
@@ -11,22 +11,19 @@ namespace Palette3D
 	}
 
 
-	Vec2 Vec2::operator+(const Vec2& rhs) const
+	void Vec2::operator+=(const Vec2 & rhs)
 	{
-		return Vec2(mX + rhs.mX, mY + rhs.mY);
+		*this = *this + rhs;
 	}
 
-
-	Vec2 Vec2::operator-(const Vec2& rhs) const
+	void Vec2::operator-=(const Vec2 & rhs)
 	{
-		return Vec2(mX - rhs.mX, mY - rhs.mY);
+		*this = *this - rhs;
 	}
 
-
-	Vec2 Vec2::operator*(const float& rhs) const
+	void Vec2::operator*=(const float & rhs)
 	{
-		return Vec2(mX * rhs, mY * rhs);
-
+		*this = *this * rhs;
 	}
 
 	Vec2 Vec2::cross(const Vec2 & other) const
@@ -42,5 +39,21 @@ namespace Palette3D
 	float Vec2::magnitude() const
 	{
 		return static_cast<float>(sqrt(powf(mX, 2) + powf(mY, 2)));
+	}
+	float Vec2::squaredMagnitude() const
+	{
+		return static_cast<float>((powf(mX, 2) + powf(mY, 2)));
+	}
+	float Vec2::distance(const Vec2 & other) const
+	{
+		return static_cast<float>(sqrt(powf(other.mX - mX, 2) + powf(other.mY - mY, 2)));
+	}
+	float Vec2::squaredDistance(const Vec2& other) const
+	{
+		return static_cast<float>(powf(other.mX - mX, 2) + powf(other.mY - mY, 2));;
+	}
+	std::ostream & operator<<(std::ostream & os, const Vec2 & v)
+	{
+		return os << "Vector2(" << v.mX << ", " << v.mY <<")"<<std::endl;
 	}
 }
