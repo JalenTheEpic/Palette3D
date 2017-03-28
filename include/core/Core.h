28d2f81d67ce7772cb0530cb486a9ef6c04ef837
JalenTheEpic <fearthedurp@gmail.com>
2017-03-28 13:30:19 -0400

    Add convenience .h files

---
+++ include/core/Core.h
@@ -0,0 +1,12 @@
+/*
+Palette3D
+Copyright (c) 2017 Jalen Cureton, William Buell
+Licensed under the terms of the MIT License (see LICENSE.txt)
+*/
+
+#ifndef _CORE_CORE_
+#define _CORE_CORE_
+
+#include <core\math\Math.h>
+
+#endif
---
+++ include/core/math/Math.h
@@ -0,0 +1,16 @@
+/*
+Palette3D
+Copyright (c) 2017 Jalen Cureton, William Buell
+Licensed under the terms of the MIT License (see LICENSE.txt)
+*/
+
+#ifndef _CORE_MATH_MATH_
+#define _CORE_MATH_MATH_
+
+#include <core\math\Matrix3.h>
+#include <core\math\Matrix4.h>
+#include <core\math\Vec2.h>
+#include <core\math\Vec3.h>
+#include <core\math\Vec4.h>
+
+#endif
