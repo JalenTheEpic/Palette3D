73033e9b64e91ece08640ac31071c5b609a971ff
JalenTheEpic <fearthedurp@gmail.com>
2017-03-30 17:12:38 -0400

    Add float shorthand

--- include/core/atomic/Types.h
+++ include/core/atomic/Types.h
@@ -2,6 +2,7 @@
 #define _CORE_ATOMIC_TYPES_
 
 #include <stdint.h>
+
 typedef int8_t I8;
 typedef int16_t I16;
 typedef int32_t I32;
@@ -12,6 +13,9 @@ typedef uint8_t U8;
 typedef uint16_t U16;
 typedef uint32_t U32;
 typedef uint64_t U64;
+
+typedef float F32;
+typedef double F64;
 
 
 #endif
