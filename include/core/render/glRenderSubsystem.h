57202b7371da41bf29e7f1de640e7647b2126255
JalenTheEpic <fearthedurp@gmail.com>
2017-04-06 13:48:47 -0400

    Add gl render subsystem files

---
+++ include/core/render/glRenderSubsystem.h
@@ -0,0 +1,40 @@
+/*
+Palette3D
+Copyright (c) 2017 Jalen Cureton, William Buell
+Licensed under the terms of the MIT License (see LICENSE.txt)
+*/
+
+
+#ifndef _CORE_RENDER_GLRENDERSUBSYSTEM_
+#define _CORE_RENDER_GLRENDERSUBSYSTEM_
+
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
+namespace Palette3D
+{
+	class GlRenderSubSystem
+	{
+	private:
+		void initGL();
+	public:
+		GlRenderSubSystem();
+		~GlRenderSubSystem();
+
+
+
+	};
+
+
+
+
+	template<typename T>
+	class VkDeleter {
+
+	};
+
+}
+
+#endif // !_CORE_RENDER_GLRENDERSUBSYSTEM_
