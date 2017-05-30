23bb4615b9ee10dd77da191e4881f3336b36a33b
JalenTheEpic <fearthedurp@gmail.com>
2017-05-29 22:30:50 -0400

    update

---
+++ include/core/render/gl/P3DGL.h
@@ -0,0 +1,15 @@
+#ifndef _CORE_RENDER_GL_P3DGL_
+#define _CORE_RENDER_GL_P3DGL_
+
+#include<core\render\gl\Cube.h>
+#include<core\render\gl\Square.h>
+#include<core\render\gl\Program.h>
+#include<core\render\gl\Texture.h>
+#include<core\render\gl\glRenderSubsystem.h>
+
+
+
+
+
+#endif // !_CORE_RENDER_GL_P3DGL_
+
